fb4f300ed2a9df88d03eabc465806cc0d6116e15
Rupam Das <rejisterjack@gmail.com>
2025-04-20 20:55:13 +0530

    feat: connections

--- devtinder-server/src/routes/user.js
+++ devtinder-server/src/routes/user.js
@@ -23,9 +23,19 @@ router.get("/user/connections", validateUser, async (req, res) => {
         { fromUserId: req.user._id, status: "accepted" },
         { toUserId: req.user._id, status: "accepted" },
       ],
-    }).populate("fromUserId", "firstName lastName emailId")
+    })
+      .populate("fromUserId", "firstName lastName emailId")
+      .populate("toUserId", "firstName lastName emailId")
 
-    res.status(200).json(connections)
+    const data = connections.map((row) => {
+      if (row.fromUserId._id.toString() === req.user._id.toString()) {
+        return row.toUserId
+      } else {
+        return row.fromUserId
+      }
+    })
+
+    res.status(200).json(data)
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
